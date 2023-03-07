ab7330553d9952d9fb13f057d8648b47ae6b35f6
Dagore <david.gomez.redondo.dev@gmail.com>
2023-03-07 15:51:12 +0100

    logout workouts cleanup

--- src/hooks/useLogout.tsx
+++ src/hooks/useLogout.tsx
@@ -1,11 +1,14 @@
 import useAuthContext from './useAuthContext'
+import useWorkoutsContext from './useWorkoutsContext'
 
 export default function useLogout (): { logout: () => void } {
   const [, authDispatch] = useAuthContext()
+  const [, workoutsDispatch] = useWorkoutsContext()
 
   function logout (): void {
     localStorage.removeItem('user')
     authDispatch({ type: 'LOGOUT', payload: null })
+    workoutsDispatch({ type: 'SET_WORKOUTS', payload: [] })
   }
 
   return { logout }
