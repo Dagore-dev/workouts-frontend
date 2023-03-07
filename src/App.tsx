590acf59812dc32cfe0424089e8e3a39e5cf3a12
Dagore <david.gomez.redondo.dev@gmail.com>
2023-03-07 15:16:31 +0100

    router updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,21 @@ import './index.css'
 import {
   BrowserRouter,
   Routes,
-  Route
+  Route,
+  Navigate
 } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
+import useAuthContext from './hooks/useAuthContext'
 
 export default function App (): JSX.Element {
+  const [authState] = useAuthContext()
+  function isUserIsLogged (): boolean {
+    return authState.user != null
+  }
+
   return (
     <div className='App'>
       <BrowserRouter>
@@ -17,9 +24,9 @@ export default function App (): JSX.Element {
 
         <main className='pages'>
           <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/signup' element={<Signup />} />
+            <Route path='/' element={isUserIsLogged() ? <Home /> : <Navigate to='/login' />} />
+            <Route path='/login' element={isUserIsLogged() ? <Navigate to='/' /> : <Login />} />
+            <Route path='/signup' element={isUserIsLogged() ? <Navigate to='/' /> : <Signup />} />
           </Routes>
         </main>
       </BrowserRouter>
